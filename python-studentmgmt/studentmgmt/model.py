ae4d363b2089d32dd4fb124cd5f82d930f9b29a8
desrop <dean.desrosiers@gmail.com>
2019-03-21 13:35:29 -0400

    Put model back the way it was.

--- python-studentmgmt/studentmgmt/model.py
+++ python-studentmgmt/studentmgmt/model.py
@@ -1,26 +1,13 @@
 from studentmgmt import db
 from studentmgmt import ma
 
+# https://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html#many-to-many
+
 enrollments = db.Table("enrollment",
     db.Column("course_id", db.Integer, db.ForeignKey("course.course_id"), primary_key=True),
     db.Column("student_id", db.Integer, db.ForeignKey("student.student_id"), primary_key=True)
 )
-
-class Student(db.Model):
-    id = db.Column(db.Integer, db.Sequence("student_id_seq"), primary_key=True)
-    student_num = db.Column(db.String, unique=True, nullable=False)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
-
-    # 'courses' is a backref defined in Course
-
-    def __repr__(self):
-        return f"<Student(student_num={self.student_num!r} name={self.last_name!r}, {self.first_name!r})>"
-
-class StudentSchema(ma.ModelSchema):
-    class Meta:
-        model = Student
-        
+       
 class Course(db.Model):
     course_id = db.Column(db.Integer, db.Sequence("course_id_seq"), primary_key=True)
     course_num = db.Column(db.String, nullable=False)
@@ -28,8 +15,7 @@ class Course(db.Model):
     start_date = db.Column(db.Date, nullable=False)
     description = db.Column(db.String(256), nullable=False, default="Course details TK")
     
-    students = db.relationship("Student", secondary=enrollments, lazy="subquery",
-        backref=db.backref("courses", lazy=True))
+    students = db.relationship("Student", secondary=enrollments, back_populates="courses")
     
     def __repr__(self):
         return f"<Course(course_num={self.course_num!r} title={self.title!r})>"
@@ -38,3 +24,17 @@ class CourseSchema(ma.ModelSchema):
     class Meta:
         model = Course
 
+class Student(db.Model):
+    id = db.Column(db.Integer, db.Sequence("student_id_seq"), primary_key=True)
+    student_num = db.Column(db.String, unique=True, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+
+    courses = db.relationship("Course", secondary=enrollments, back_populates="students")
+
+    def __repr__(self):
+        return f"<Student(student_num={self.student_num!r} name={self.last_name!r}, {self.first_name!r})>"
+
+class StudentSchema(ma.ModelSchema):
+    class Meta:
+        model = Student
