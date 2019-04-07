e3213b7be5fc2f2e005219f977d4b3af3883f3b3
Dean Des Rosiers <dean.desrosiers@gmail.com>
2019-04-07 11:52:59 -0400

    Not sure, but moved to vscode settings

--- python-studentmgmt/studentmgmt/__init__.py
+++ python-studentmgmt/studentmgmt/__init__.py
@@ -2,10 +2,12 @@ import os
 from datetime import datetime
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = '712a59c234ec4b9c54e0f09277d83e13'
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://postgres:{os.environ.get('DB_CONNECT')}@studentmgmt-db:5432/studentmgmt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
